f38135be6f6efc51cf83e3e435bcf234a86b6744
Zach Jacobson <jacobsonzacharym@gmail.com>
2023-07-25 13:29:06 -0700

    fix rust errors

--- sdks/rust/examples/main.rs
+++ sdks/rust/examples/main.rs
@@ -1,21 +1,16 @@
 use rust_sdk::{builder::{ self}, proto::{cluster_service_client::ClusterServiceClient, self}};
-use std::{future::Future, process::Output};
 use tonic::transport::Channel;
-use futures::{stream::FuturesOrdered, TryFutureExt};
+
 
 #[tokio::main]
 async fn main() {
-    builder::await_build(build);
+    builder::await_build(build).await.expect("Error in build");
 }
 
-fn build<Fut>(futs: &mut FuturesOrdered<Fut>, client: &mut ClusterServiceClient<Channel>) -> Result<tonic::Response<proto::ApiResponse>, tonic::Status>
-where
-    Fut: Future<Output = Result<tonic::Response<proto::ApiResponse>, tonic::Status>>,
-{
-    let fut1 = client.create_cluster(proto::CreateClusterRequest{id: String::from("test-from-rust"), name: None});
-    futs.push_back(fut1);
-    let fut2 = fut1.and_then(|_| client.add_teams(proto::AddTeamsRequest{teams: vec![proto::Team{team: Some(proto::team::Team::GenericTeam(proto::GenericTeam{name: String::from("hello")}))}]}));
-    let fut3 = fut2.and_then(|_| client.add_addons(proto::AddAddonsRequest{addons: vec![proto::Addon{addon: Some(proto::addon::Addon::AckAddOn(proto::AckAddOn{id: Some(String::from("hi")), service_name: String::from("eks")}))}]}));
-    let fut4 = fut3.and_then(|_| client.build_cluster(proto::BuildClusterRequest{cluster_name: String::from("test-from-rust"), account: None, region:None}));
-    fut4.await
+async fn build(client: &mut ClusterServiceClient<Channel>) -> Result<tonic::Response<proto::ApiResponse>, tonic::Status> {
+    client.create_cluster(proto::CreateClusterRequest{id: String::from("test-from-rust"), name: None}).await?;
+    client.add_teams(proto::AddTeamsRequest{teams: vec![proto::Team{team: Some(proto::team::Team::GenericTeam(proto::GenericTeam{name: String::from("hello")}))}]}).await?;
+    client.add_addons(proto::AddAddonsRequest{addons: vec![proto::Addon{addon: Some(proto::addon::Addon::AckAddOn(proto::AckAddOn{id: Some(String::from("hi")), service_name: String::from("eks")}))}]}).await?;
+    client.build_cluster(proto::BuildClusterRequest{cluster_name: String::from("test-from-rust"), account: None, region:None}).await
+
 }
--- sdks/rust/src/builder.rs
+++ sdks/rust/src/builder.rs
@@ -1,25 +1,16 @@
-use std::{future::Future, error::Error};
-
-use futures::{stream::FuturesOrdered, StreamExt};
+use async_fn_traits::AsyncFnOnce1;
 use tonic::transport::Channel;
 
 use crate::proto::{cluster_service_client::ClusterServiceClient, self};
 
-pub async fn await_build<F, Fut>(f: F) -> Result<(), Box<dyn Error + Send + Sync + 'static>>
+
+pub async fn await_build<F>(f: F) -> Result<tonic::Response<proto::ApiResponse>,tonic::Status> 
 where
-    F: FnOnce(&mut FuturesOrdered<Fut>, &mut ClusterServiceClient<Channel>) -> Result<tonic::Response<proto::ApiResponse>, tonic::Status> + Send +'static,
-    Fut: Future<Output = Result<tonic::Response<proto::ApiResponse>, tonic::Status>>,
+    F: for<'a> AsyncFnOnce1<&'a mut ClusterServiceClient<Channel>, Output = Result<tonic::Response<proto::ApiResponse>, tonic::Status>>
 {
-    let channel = tonic::transport::Channel::from_static("http://[::1]:50051").connect().await?;
-    let mut futures_list = FuturesOrdered::new();
+    let channel = tonic::transport::Channel::from_static("http://[::1]:50051").connect().await.expect("no connect");
     let mut client = ClusterServiceClient::new(channel);
 
-    let res = f(&mut futures_list, &mut client);
-    res?;
-    let r = futures_list.collect::<Vec<Result<tonic::Response<proto::ApiResponse>, tonic::Status>>>().await;
-    for result in r {
-        result?;
-    }
-
-    Ok(())
+    let res = f(&mut client).await;
+    res
 }
